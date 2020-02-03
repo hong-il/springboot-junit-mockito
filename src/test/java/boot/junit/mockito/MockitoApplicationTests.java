53762b372c286e63d5e26ab304540f4087e807fb
KIM HONGIL <clroyan01@gmail.com>
2020-02-03 23:06:39 +0900

    Create mockito project

---
+++ src/test/java/boot/junit/mockito/MockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package boot.junit.mockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
